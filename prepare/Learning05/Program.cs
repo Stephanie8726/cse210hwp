563cb1b739570bbc2a92988afce3d744dc6b6b26
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2023-11-19 04:08:32 -0800

    program

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,50 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Tracker tracker = new Tracker();
+
+        while (true)
+        {
+            Console.WriteLine("Menu Options:");
+            Console.WriteLine("1. Create new goal");
+            Console.WriteLine("2. List Goals");
+            Console.WriteLine("3. Save Goals");
+            Console.WriteLine("4. Load Goals");
+            Console.WriteLine("5. Record Event");
+            Console.WriteLine("6. Quit");
+
+            Console.Write("Select a choice from the menu: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    tracker.CreateGoal();
+                    break;
+                case "2":
+                    tracker.DisplayGoals();
+                    break;
+                case "3":
+                    tracker.SaveProgress();
+                    break;
+                case "4":
+                    tracker.LoadProgress();
+                    break;
+                case "5":
+                    tracker.RecordEvent();
+                    break;
+                case "6":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    break;
+            }
+        }
     }
 }
